6cfa00ba92a4ac7fa0eba4f43f1e100c7676ed25
Joe Kadi <joeekadi@gmail.com>
2020-11-20 21:55:38 +0000

    refactored to object orientated

--- pyTorch/linearmdpfrequency.py
+++ pyTorch/linearmdpfrequency.py
@@ -15,15 +15,13 @@ def linearmdpfrequency(mdp_data,p,initD):
         RHS0 = Dp[...,None].repeat((1,actions,transitions))
         RHS = RHS0*mdp_data['discount']
         Dpi = torch.mul(LHS, RHS)
-
-
         D_CSR = sps.csc_matrix((torch.flatten(Dpi.permute(1,0,2)), torch.flatten(mdp_data['sa_s'].permute(1,0,2)),torch.arange(states*actions*transitions+1)), shape=(states,states*actions*transitions)) 
         D_CSR.eliminate_zeros()
         D_mx = torch.matmul(torch.tensor(D_CSR.todense()), torch.ones(states*actions*transitions,1, dtype=torch.double))
         D_s = torch.sum(D_mx,1)
         D = initD + D_s
         D = torch.reshape(D, (len(D),1))
-        diff = max(abs(D-Dp))
+        diff = torch.max(torch.abs(D-Dp))
 
     return D
 """
--- pyTorch/main.py
+++ pyTorch/main.py
@@ -11,195 +11,157 @@ from torch.autograd import Variable
 import matplotlib.pyplot as plt
 torch.set_printoptions(precision=3)
 
+class testers:
+	def checkgradients(lh, mdp_params, k):
+		#checking gradient for k reward function points
 
-def checkgradients(lh, mdp_params, k):
-	#checking gradient for k reward function points
+		rewardsToCheck = []
+		for i in range(k):
+			checkR = np.random.randn(mdp_params['n']**2,1) #initial estimated R
+			rewardsToCheck.append(checkR)
 
-	rewardsToCheck = []
-	for i in range(k):
-		checkR = np.random.randn(mdp_params['n']**2,1) #initial estimated R
-		rewardsToCheck.append(checkR)
+		print("... checking gradient ...\n")
 
-	print("... checking gradient ...\n")
+		total_rmse = []
+		true_gradients = []
+		expected_gradients = []
 
-	total_rmse = []
-	true_gradients = []
-	expected_gradients = []
+		for reward in rewardsToCheck:
+			rmse, true_grad, expected_grad = check_grad(lh.negated_likelihood, lh.calc_gradient, [reward], epsilon = 1e-4)
+			total_rmse.append(rmse)
+			true_gradients.append(true_grad.item())
+			expected_gradients.append(expected_grad)
 
-	for reward in rewardsToCheck:
-		rmse, true_grad, expected_grad = check_grad(lh.negated_likelihood, lh.calc_gradient, [reward], epsilon = 1e-4)
-		total_rmse.append(rmse)
-		true_gradients.append(true_grad.item())
-		expected_gradients.append(expected_grad)
+		#print and plot RMSE
+		print('\nGradient check terminated with a RMSE of {} from {} gradient checks\n'.format(sum(total_rmse)/len(total_rmse), len(rewardsToCheck)))
+		plt.plot(true_gradients, expected_gradients, 'bo')
+		plt.title('check_grad() gradient comparison')
+		plt.xlabel('True Gradients')
+		plt.ylabel('Expected Gradients')
+		plt.grid(b='True', which='minor')
+		plt.show()
 
-	#print and plot RMSE
-	print('\nGradient check terminated with a RMSE of {} from {} gradient checks\n'.format(sum(total_rmse)/len(total_rmse), len(rewardsToCheck)))
-	plt.plot(true_gradients, expected_gradients, 'bo')
-	plt.title('check_grad() gradient comparison')
-	plt.xlabel('True Gradients')
-	plt.ylabel('Expected Gradients')
-	plt.grid(b='True', which='minor')
-	plt.show()
+	def test_gradient(lh, testr):
+		print('Gradient for test r is \n{}'.format(lh.calc_gradient(testr)))
+		return(lh.calc_gradient(testr))
 
-def test_gradient(lh, testr):
-	print('Gradient for test r is \n{}'.format(lh.calc_gradient(testr)))
-	return(lh.calc_gradient(testr))
+	def test_likelihood(lh, testr):
+		print('Likelihood for test r is {}'.format(lh.negated_likelihood(testr)))
+		return(lh.negated_likelihood(testr))
 
-def test_likelihood(lh, testr):
-	print('Likelihood for test r is \n{}'.format(lh.negated_likelihood(testr)))
-	return(lh.negated_likelihood(testr))
+	def compare_gandLH_with_matlab(lh):
+		torchG = test_gradient(lh, testr)
+		torchL =test_likelihood(lh, testr)
 
-def compare_gandLH_with_matlab(lh, testr):
-	torchG = test_gradient(lh, testr)
-	torchL =test_likelihood(lh, testr)
+		testr = np.array(
+		[[5.11952e+01],
+		[2.17734e+05],
+		[1.01630e+0],
+		[1.44944e-07]])
+		matlabG = np.array([[-0227.937600000000],
+							[8139.016753098902],
+							[-3837.240000000000],
+							[-4073.850000000000]])
+		  
+		matlabL = 1.772136688141655e+09
 
+		print('Elementwise diff torch gradient - matlab gradient is \n {}'.format(np.subtract(torchG.detach().cpu().numpy(),matlabG)))
+		print('Likelihood diff is {}'.format(torchL - matlabL))
 
-	matlabG = np.array([[-0227.937600000000],
-						[8139.016753098902],
-						[-3837.240000000000],
-						[-4073.850000000000]])
-	  
-	matlabL = 1.772136688141655e+09
+class minimisers:
+	def minimise_likelihood_with_torch(self, lh):
+		#minimsing likelihood for true R
+		print("... minimising likelihood for R ...\n")
 
-	print('Elementwise diff torch gradient - matlab gradient is \n {}'.format(np.subtract(torchG.detach().cpu().numpy(),matlabG)))
-	print('Likelihood diff is {}'.format(torchL - matlabL))
+		#used for lfbgs 
+		def closure():
+		    optimizer.zero_grad()
+		    loss = lh.negated_likelihood(estR)
+		    estR.grad = lh.calc_gradient(estR)
+		    return loss
 
-def minimise_likelihood_with_torch():
-	#minimsing likelihood for true R
-	print("... minimising likelihood for R ...\n")
+		estR = torch.randn(mdp_params['n']**2,1, dtype=torch.float64, requires_grad=True) #initial estimated R
+		countlist = []
+		NLLlist = []
+		gradList = []
+		estRlist = []
+		lr = 0.5
+		n_epochs = 1000
+		NLL = 0
+		prev = 0
+		diff = 10
+		threshhold =  0.00000001
+		i = 0
 
-	#used for lfbgs 
-	def closure():
-	    optimizer.zero_grad()
-	    loss = lh.negated_likelihood(estR)
-	    estR.grad = lh.calc_gradient(estR)
-	    return loss
+		optimizer = torch.optim.Adam([estR], lr=lr, eps = 1e-6)
+		while (diff >= threshhold):
+			i += 1
+			prev = NLL
+			optimizer.zero_grad()
+			NLL = lh.negated_likelihood(estR)
+			#NLL.backward()
+			estR.grad = lh.calc_gradient(estR)
+			optimizer.step()
+			print('Optimiser iteration {} with NLL {}, summed estR values of {} and summed gradient of{}\n'.format(i, NLL, torch.sum(estR.data), torch.sum(estR.grad)))
+			diff = abs(prev-NLL)
+			#store values for plotting
+			gradList.append(torch.sum(estR.grad))
+			NLLlist.append(NLL)
+			countlist.append(i)
+			estRlist.append(torch.sum(estR.data))
 
-	estR = torch.randn(mdp_params['n']**2,1, dtype=torch.float64, requires_grad=True) #initial estimated R
-	countlist = []
-	NLLlist = []
-	gradList = []
-	estRlist = []
-	lr = 0.5
-	n_epochs = 1000
-	NLL = 0
-	prev = 0
-	diff = 10
-	threshhold =  0.00000001
-	i = 0
-
-	optimizer = torch.optim.Adam([estR], lr=lr)
-	while (diff >= threshhold):
-		i += 1
-		prev = NLL
-		optimizer.zero_grad()
-		NLL = lh.negated_likelihood(estR)
-		#NLL.backward()
-		estR.grad = lh.calc_gradient(estR)
-		optimizer.step()
-		print('Optimiser iteration {} with NLL {}, estR values of \n{} and gradient \n {}'.format(i, NLL, estR.data, estR.grad))
-
-		diff = abs(prev-NLL)
-
-		#store values for plotting
-		gradList.append(torch.sum(estR.grad))
-		NLLlist.append(NLL)
-		countlist.append(i)
-		estRlist.append(torch.sum(estR.data))
-
-	#Normalise plot data
-	NLLlist = [float(i)/sum(NLLlist) for i in NLLlist]
-	gradList = [float(i)/sum(gradList) for i in gradList]
-	estRlist = [float(i)/sum(estRlist) for i in estRlist]
-
-
-	#normalizedNLL = (NLLlist-min(NLLlist))/(max(NLLlist)-min(NLLlist))
-	f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex=True)
-	ax1.plot(countlist, NLLlist)
-	ax1.set_title('Likelihood')
-	ax2.plot(countlist, gradList)
-	ax2.set_title('grad')
-	ax3.plot(countlist, estRlist)
-	ax3.set_title('estR')
+		#Normalise plot data
+		NLLlist = [float(i)/sum(NLLlist) for i in NLLlist]
+		gradList = [float(i)/sum(gradList) for i in gradList]
+		estRlist = [float(i)/sum(estRlist) for i in estRlist]
 
 
-	'''
-	plt.plot(countlist, NLLlist, label = "likelihood")
-	plt.plot(countlist, NLLlist, 'bo' )
-	plt.plot(countlist, gradList, label = "gradient")
-	plt.plot(countlist, gradList, 'r+')
-	plt.plot(countlist, estRlist, label = "estR")
-	plt.plot(countlist, estRlist, 'y*')
-	plt.ylabel('Normalised Value')
-	plt.xlabel('Iterations')
-	plt.grid(b='True', which='minor')
-	plt.legend()
-	'''
-
-	plt.show()
+		#normalizedNLL = (NLLlist-min(NLLlist))/(max(NLLlist)-min(NLLlist))
+		f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex=True)
+		ax1.plot(countlist, NLLlist)
+		ax1.set_title('Likelihood')
+		ax2.plot(countlist, gradList)
+		ax2.set_title('grad')
+		ax3.plot(countlist, estRlist)
+		ax3.set_title('estR')
+		plt.show()
 
 
-	print('\n *** RESULTS AFTER OPTIMISER CONVERGES *** \n')
-	print("loglik =\n", NLL.data.numpy(), "\nr =\n", r.data.numpy())
-	    
+		#reshape foundR & find it's likelihood
+		foundR = torch.reshape(torch.tensor(estR.data), (4,1))
+		foundR = foundR.repeat(1, 5)
+		print(foundR.dtype)
+		foundLH = lh.negated_likelihood(foundR)
+
+		#solve MDP with foundR for optimal policy
+		v, q, logp, foundp = linearvalueiteration(mdp_data, foundR)
+		found_optimal_policy = np.argmax(foundp.detach().cpu().numpy(), axis=1) 
 
 
-	#reshape foundR & find it's likelihood
-	foundR = torch.reshape(torch.tensor(estR.data), (4,1))
-	foundR = foundR.repeat(1, 5)
-	print(foundR.dtype)
-	foundLH = lh.negated_likelihood(foundR)
+		print("\nTrue R is \n{}\n with negated likelihood of {}\n and optimal policy {}\n".format(*trueRprintlist))
 
-	#solve MDP with foundR for optimal policy
-	v, q, logp, foundp = linearvalueiteration(mdp_data, foundR)
-	found_optimal_policy = np.argmax(foundp.detach().cpu().numpy(), axis=1) 
+		#Print found R stats
+		foundRprintlist = [foundR, foundLH, found_optimal_policy]
+		print("\nFound R is \n{}\n with negated likelihood of {}\n and optimal policy {}\n".format(*foundRprintlist))
 
+	def minimise_likelihood_with_scipy(self, lh):
+		res = minimize(lh.negated_likelihood, guess, jac=True, method="L-BFGS-B", options={'disp': True, 'gtol': 1e-05, 'eps': 1e-08, 'maxiter': 15000, 'ftol': 2.220446049250313e-09, 'maxcor': 10, 'maxfun': 15000})
+		#reshape foundR & find it's likelihood
+		foundR = torch.reshape(torch.tensor(res.x), (4,1))
+		foundR = foundR.repeat(1, 5)
+		print(foundR.dtype)
+		foundLH = lh.negated_likelihood(foundR)
 
-	print("\nTrue R is \n{}\n with negated likelihood of {}\n and optimal policy {}\n".format(*trueRprintlist))
-
-	#Print found R stats
-	foundRprintlist = [foundR, foundLH, found_optimal_policy]
-	print("\nFound R is \n{}\n with negated likelihood of {}\n and optimal policy {}\n".format(*foundRprintlist))
-
-def minimise_likelihood_with_scipy(lh):
-	res = minimize(lh.negated_likelihood, guess, jac=True, method="L-BFGS-B", options={'disp': True, 'gtol': 1e-05, 'eps': 1e-08, 'maxiter': 15000, 'ftol': 2.220446049250313e-09, 'maxcor': 10, 'maxfun': 15000})
-
-def scaleR():
-	#code to evaluate consistency when scaling R
-	print("\n ... Doubling R & recalcuating ... \n")
-	doubledR = r*2
-	#solve MDP
-	v, q, logp, p = linearvalueiteration(mdp_data, doubledR)
-	mdp_solution = {'v': v, 'q':q, 'p':p, 'logp': logp}
-	optimal_policy = Torch.argmax(p, dim=1)   
-
-	#set likelihood variables
-	lh = Likelihood()
-	lh.set_variables_for_likehood(mdp_data, N, T, example_samples)
+		#solve MDP with foundR for optimal policy
+		v, q, logp, foundp = linearvalueiteration(mdp_data, foundR)
+		found_optimal_policy = np.argmax(foundp.detach().cpu().numpy(), axis=1) 
 
 
-	doubledLH = lh.negated_likelihood(doubledR)
-	doubledRprintlist = [doubledR, trueLH,optimal_policy]
-	print("\nDouble R is \n{}\n with negated likelihood of {}\n and optimal policy {}".format(*doubledRprintlist))
+		print("\nTrue R is \n{}\n with negated likelihood of {}\n and optimal policy {}\n".format(*trueRprintlist))
 
-
-	print("\n ... Quadrupling R & recalculating ... \n")
-	doubledR = r*4
-	#solve MDP
-	v, q, logp, p = linearvalueiteration(mdp_data, doubledR)
-	mdp_solution = {'v': v, 'q':q, 'p':p, 'logp': logp}
-	optimal_policy = Torch.argmax(p, dim=1)   
-
-
-	#set likelihood variables
-	lh = Likelihood()
-	lh.set_variables_for_likehood(mdp_data, N, T, example_samples)
-
-
-	doubledLH = lh.negated_likelihood(doubledR)
-	doubledRprintlist = [doubledR, trueLH,optimal_policy]
-	print("\nQuadrupled R is \n{}\n with negated likelihood of {}\n and optimal policy {}".format(*doubledRprintlist))
-
+		#Print found R stats
+		foundRprintlist = [foundR, foundLH, found_optimal_policy]
+		print("\nFound R is \n{}\n with negated likelihood of {}\n and optimal policy {}\n".format(*foundRprintlist))
 
 
 #set mdp params
@@ -235,141 +197,10 @@ example_samples = sampleexamples(N,T, mdp_solution, mdp_data)
 lh = Likelihood()
 lh.set_variables_for_likehood(mdp_data, N, T, example_samples)
 
-#testreward
-testr = np.array(
-	[[5.11952e+01],
-	[2.17734e+05],
-	[1.01630e+0],
-	[1.44944e-07]])
-
-compare_gandLH_with_matlab(lh,testr)
-  
-  
-minimise_likelihood_with_torch()
+mnms = minimisers()
+mnms.minimise_likelihood_with_torch(lh)
   
 
-'''
-rmse, true_grad, expected_grad = check_grad(lh.negated_likelihood, lh.calc_gradient, [testr], epsilon = 1e-4)
-
-print('True grad of \n {} \n expected grad of \n {} \n with a rmse of \n{}'.format(true_grad, expected_grad, rmse))
-
-
-
-checkgradients(lh, mdp_params, 10)
-
-
-#minimise_likelihood_with_torch() -- add appropiate parameters to method signature and call
-
-#Get true R values
-trueLH = lh.negated_likelihood(r)
-trueRprintlist = [r, trueLH, optimal_policy]
-estR = torch.randn(mdp_params['n']**2,1, dtype=torch.float64, requires_grad=True) #initial estimated R
-'''
-"""
-#minimsing likelihood for true R
-print("... minimising likelihood for R ...\n")
-
-
-
-#used for lfbgs 
-def closure():
-    optimizer.zero_grad()
-    loss = lh.negated_likelihood(estR)
-    estR.grad = lh.calc_gradient(estR)
-    return loss
-
-
-countlist = []
-NLLlist = []
-gradList = []
-estRlist = []
-lr = 0.5
-n_epochs = 1000
-NLL = 0
-prev = 0
-diff = 10
-threshhold =  0.00000001
-i = 0
-
-optimizer = torch.optim.Adam([estR], lr=lr)
-while (diff >= threshhold):
-	i += 1
-	prev = NLL
-	optimizer.zero_grad()
-	NLL = lh.negated_likelihood(estR)
-	#NLL.backward()
-	estR.grad = lh.calc_gradient(estR)
-	optimizer.step()
-	print('Optimiser iteration {} with NLL {}, estR values of \n{} and gradient \n {}'.format(i, NLL, estR.data, estR.grad))
-
-	diff = abs(prev-NLL)
-
-	#store values for plotting
-	gradList.append(torch.sum(estR.grad))
-	NLLlist.append(NLL)
-	countlist.append(i)
-	estRlist.append(torch.sum(estR.data))
-
-#Normalise plot data
-NLLlist = [float(i)/sum(NLLlist) for i in NLLlist]
-gradList = [float(i)/sum(gradList) for i in gradList]
-estRlist = [float(i)/sum(estRlist) for i in estRlist]
-
-
-#normalizedNLL = (NLLlist-min(NLLlist))/(max(NLLlist)-min(NLLlist))
-f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex=True)
-ax1.plot(countlist, NLLlist)
-ax1.set_title('Likelihood')
-ax2.plot(countlist, gradList)
-ax2.set_title('grad')
-ax3.plot(countlist, estRlist)
-ax3.set_title('estR')
-
-
-'''
-plt.plot(countlist, NLLlist, label = "likelihood")
-plt.plot(countlist, NLLlist, 'bo' )
-plt.plot(countlist, gradList, label = "gradient")
-plt.plot(countlist, gradList, 'r+')
-plt.plot(countlist, estRlist, label = "estR")
-plt.plot(countlist, estRlist, 'y*')
-plt.ylabel('Normalised Value')
-plt.xlabel('Iterations')
-plt.grid(b='True', which='minor')
-plt.legend()
-'''
-
-plt.show()
-
-
-print('\n *** RESULTS AFTER OPTIMISER CONVERGES *** \n')
-print("loglik =\n", NLL.data.numpy(), "\nr =\n", r.data.numpy())
-    
-
-
-#reshape foundR & find it's likelihood
-foundR = torch.reshape(torch.tensor(estR.data), (4,1))
-foundR = foundR.repeat(1, 5)
-print(foundR.dtype)
-foundLH = lh.negated_likelihood(foundR)
-
-#solve MDP with foundR for optimal policy
-v, q, logp, foundp = linearvalueiteration(mdp_data, foundR)
-found_optimal_policy = np.argmax(foundp.detach().cpu().numpy(), axis=1) 
-
-
-print("\nTrue R is \n{}\n with negated likelihood of {}\n and optimal policy {}\n".format(*trueRprintlist))
-
-#Print found R stats
-foundRprintlist = [foundR, foundLH, found_optimal_policy]
-print("\nFound R is \n{}\n with negated likelihood of {}\n and optimal policy {}\n".format(*foundRprintlist))
-
-
-
-
-
-
-"""
 
 
 
