1540388875ce90dbd3baa8ba8994e58173ea96a8
Joe Kadi <35597975+joekadi@users.noreply.github.com>
2021-04-16 13:30:44 +0100

    Update mcd_total_train.py

--- pyTorch/train_models/total_uncertainty/mcd_total_train.py
+++ pyTorch/train_models/total_uncertainty/mcd_total_train.py
@@ -216,7 +216,8 @@ if __name__ == "__main__":
     train_loader = torch.utils.data.DataLoader(feature_data['splittable'], num_workers = 8)
     
     # Define trainer
-    trainer = pl.Trainer(max_epochs=configuration_dict['number_of_epochs'])
+    #trainer = pl.Trainer(max_epochs=configuration_dict['number_of_epochs'])
+    trainer = pl.Trainer()
 
     # Define network
     model = LitModel(len(feature_data['splittable'][0]), 'relu', configuration_dict)
